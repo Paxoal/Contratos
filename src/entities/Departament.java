3371204c1dee60c3857a9bb11557e9c28261a2c1
Arthur Paschoal <arthurpaschoal500@gmail.com>
2021-12-07 10:21:44 -0300

    Contratos

---
+++ src/entities/Departament.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Departament {
+	private String namedepartament;
+
+	public Departament() {
+
+	}
+
+	public Departament(String namedepartament) {
+		this.namedepartament = namedepartament;
+	}
+
+	public String getNamedepartament() {
+		return namedepartament;
+	}
+
+	public void setNamedepartament(String namedepartament) {
+		this.namedepartament = namedepartament;
+	}
+
+}
